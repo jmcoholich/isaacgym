a83513bbcd970d4ba6a69a64a703a26cbc44b4ba
Jeremiah Coholich <jcoholich@gatech.edu>
2022-06-17 18:26:33 -0400

    enable wandb api key

--- docker/eval_sbatch_docker_run.sh
+++ docker/eval_sbatch_docker_run.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-# wandb login dbf6e5f5f95834689237ec641e4bde9bf8f522f7
+wandb login dbf6e5f5f95834689237ec641e4bde9bf8f522f7
 wandb disabled
 cd /opt/isaacgym/python/rlgpu
 NUM_GPUS=$(nvidia-smi --query-gpu=name --format=csv,noheader | wc -l)
