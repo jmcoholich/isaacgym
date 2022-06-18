e883685038d7a72bae2d40fff69b49da6b9b5a19
Jeremiah Coholich <jcoholich@gatech.edu>
2022-06-18 13:06:36 -0400

    add code for copying over log files to personal workstation once the evaluation is finished

--- eval_submit.sh
+++ eval_submit.sh
@@ -20,3 +20,14 @@ NUM_GPUS=$(nvidia-smi --query-gpu=name --format=csv,noheader | wc -l)
 bash docker/build.sh $2
 docker rm isaacgym_container_$2
 bash docker/eval_sbatch_run.sh "$1" $2 # this script needs to do all the work
+
+# Now move the data from SkyNet to my personal workstation
+DIR_NAME=$(python -c "print('$1'.replace(' ', '_').replace('(', '').replace(')', '').replace('.', ''))")_debug
+DATA_DIR="rlgpu/python/data/$DIR_NAME"
+PWS_DIR="jcoholich@143.215.128.197:/home/jcoholich/isaacgym/python/rlgpu/data/$DIR_NAME"  # personal workstation directory
+echo Moving data from:
+echo $DATA_DIR
+echo
+echo to:
+echo $PWS_DIR
+rsync --remove-source-files -r $DATA_DIR $PWS_DIR
