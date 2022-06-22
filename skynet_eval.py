cbccc9124b71285a20bf71a5fff340d1d6f9af5f
Jeremiah Coholich <jcoholich@gatech.edu>
2022-06-22 19:34:05 -0400

    bug fix

--- skynet_eval.py
+++ skynet_eval.py
@@ -6,35 +6,40 @@ Run python/rlgpu/evluate_policy.py on skynet in a docker container with
 import subprocess
 from skynet_run import get_blacklist
 
-for cf in [25, 50, 75, 100, 150, 200]:
-    for bs in [0.15, 0.225, 0.3]:
-        run_name = "H ss state (0.2 rand new)"  # SOTA
-        #run_name = "H long steps curr 1k"
-        des_dir_coef = cf  # default: 50
-        search_box_len = bs  # default: 0.15
-        job_nickname = f"eval_{run_name}".replace(" ", "").replace(".", "").replace("(", "").replace(")", "").lower() + "dd_" + str(des_dir_coef) + "_bb_" + str(search_box_len).replace(".", "_")
-        # python_cmd = f"python evaluate_policy.py --run_name='{run_name}' --debug"
-        python_cmd = run_name
+def main():
+    for cf in [25, 50, 75, 100, 150, 200]:
+        for bs in [0.15, 0.225, 0.3]:
+            run_name = "H ss state (0.2 rand new)"  # SOTA
+            #run_name = "H long steps curr 1k"
+            des_dir_coef = cf  # default: 50
+            search_box_len = bs  # default: 0.15
+            job_nickname = f"eval_{run_name}".replace(" ", "").replace(".", "").replace("(", "").replace(")", "").lower() + "dd_" + str(des_dir_coef) + "_bb_" + str(search_box_len).replace(".", "_")
+            # python_cmd = f"python evaluate_policy.py --run_name='{run_name}' --debug"
+            python_cmd = run_name
 
-        slurm_options = [
-            "--cpus-per-gpu", "7",
-            "-p", "overcap",
-            "-A", "overcap",
-            "--constraint", "rtx_6000|a40",
-            "--gres", "gpu:8",
-            "-x", get_blacklist(),
-            #"-w", "zima, sophon, claptrap"
-        ]
+            slurm_options = [
+                "--cpus-per-gpu", "7",
+                "-p", "overcap",
+                "-A", "overcap",
+                "--constraint", "rtx_6000|a40",
+                "--gres", "gpu:8",
+                "-x", get_blacklist(),
+                #"-w", "zima, sophon, claptrap"
+            ]
 
-        # loop through random seeds
-        # for i in range(num_runs):
-        unique_name = job_nickname
-        dep_slurm_args = [
-            "-o",  unique_name + ".log",
-            "-J", unique_name
-        ]
-        cmd = ["sbatch"] + slurm_options + dep_slurm_args + ["eval_submit.sh"] \
-            + [python_cmd] + [unique_name] + [str(des_dir_coef)] + [str(search_box_len)]
-        print(" ".join(cmd))
-        print()
-        subprocess.run(cmd)
+            # loop through random seeds
+            # for i in range(num_runs):
+            unique_name = job_nickname
+            dep_slurm_args = [
+                "-o",  unique_name + ".log",
+                "-J", unique_name
+            ]
+            cmd = ["sbatch"] + slurm_options + dep_slurm_args + ["eval_submit.sh"] \
+                + [python_cmd] + [unique_name] + [str(des_dir_coef)] + [str(search_box_len)]
+            print(" ".join(cmd))
+            print()
+            subprocess.run(cmd)
+
+
+if __name__ == "__main__":
+    main()
--- skynet_run.py
+++ skynet_run.py
@@ -14,30 +14,34 @@ def get_blacklist():
     blacklist = [i.rstrip() for i in x]
     return ','.join(blacklist)
 
+def main():
+    num_runs = 5
+    job_nickname = "h_2_ahead_alt"
+    python_cmd = "python rlg_train.py --cfg_env 12_H_2_ahead_alt --cfg_train 12_large_net --wandb_project aliengo_12"
 
-num_runs = 5
-job_nickname = "h_2_ahead_alt"
-python_cmd = "python rlg_train.py --cfg_env 12_H_2_ahead_alt --cfg_train 12_large_net --wandb_project aliengo_12"
-
-slurm_options = [
-    "--cpus-per-gpu", "7",
-    "-p", "short",
-    "--constraint", "2080_ti",
-    "--gres", "gpu:1",
-    "-x", get_blacklist(),
-]
-
-# loop through random seeds
-for i in [0, 1]:
-    unique_name = job_nickname + str(i)
-    dep_slurm_args = [
-        "-o",  unique_name + ".log",
-        "-J", unique_name
+    slurm_options = [
+        "--cpus-per-gpu", "7",
+        "-p", "short",
+        "--constraint", "2080_ti",
+        "--gres", "gpu:1",
+        "-x", get_blacklist(),
     ]
-    cmd = ["sbatch"] + slurm_options + dep_slurm_args + ["submit.sh"] \
-        + [python_cmd + " --seed " + str(i)] + [unique_name]
-    print(" ".join(cmd))
-    print()
-    subprocess.run(cmd)
+
+    # loop through random seeds
+    for i in range(num_runs):
+        unique_name = job_nickname + str(i)
+        dep_slurm_args = [
+            "-o",  unique_name + ".log",
+            "-J", unique_name
+        ]
+        cmd = ["sbatch"] + slurm_options + dep_slurm_args + ["submit.sh"] \
+            + [python_cmd + " --seed " + str(i)] + [unique_name]
+        print(" ".join(cmd))
+        print()
+        subprocess.run(cmd)
+
+
+if __name__ == "__main__":
+    main()
 
 
