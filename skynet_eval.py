c9c8c7f752c832d6f3ef8c5e91cd030058afcb8c
Jeremiah Coholich <jcoholich@gatech.edu>
2022-06-28 16:30:08 -0400

    made hparam grid search go in random order instead of grid

--- skynet_eval.py
+++ skynet_eval.py
@@ -5,44 +5,47 @@ Run python/rlgpu/evluate_policy.py on skynet in a docker container with
 
 import subprocess
 from skynet_run import get_blacklist
+import itertools
+from random import shuffle
 
 def main():
-    for cf in [50, 75, 150]:  # des_dir_coef
-        # for cf in [75]:
-        for bs in [0.2, 0.225, 0.275]:  # box_len
-            # for bs in [0.15, 0.225, 0.3]:
-            for nn_ft_dist in [0.05, 0.1, 0.2, 0.4]:  # next next footstep distance for optimization
-                for nn_ft_width in [0.0, 0.075, 0.15]:  # width of nn footstep targets
-                    # run_name = "H ss state (0.2 rand new)"  # SOTA
-                    run_name = "H new sota"
-                    des_dir_coef = cf  # default: 50
-                    search_box_len = bs  # default: 0.15
-                    job_nickname = f"eval_{run_name}".replace(" ", "").replace(".", "").replace("(", "").replace(")", "").lower() + "dd_" + str(des_dir_coef) + "_bb_" + str(search_box_len).replace(".", "_") + "_dist_" + str(nn_ft_dist).replace(".", "_") + "_width_" + str(nn_ft_width).replace(".", "_")
-                    # python_cmd = f"python evaluate_policy.py --run_name='{run_name}' --debug"
-                    python_cmd = run_name
+    des_dir_coefficients = [50, 75, 150]
+    box_lengths = [0.2, 0.225, 0.275]
+    nn_ft_dists = [0.05, 0.1, 0.2, 0.4]
+    nn_ft_widths = [0.0, 0.075, 0.15]
+    prod = list(itertools.product(des_dir_coefficients, box_lengths, nn_ft_dists, nn_ft_widths))
+    shuffle(prod)
+    for cf, bs, nn_ft_dist, nn_ft_width in prod:
+        # run_name = "H ss state (0.2 rand new)"  # SOTA
+        run_name = "H new sota"
+        des_dir_coef = cf  # default: 50
+        search_box_len = bs  # default: 0.15
+        job_nickname = f"eval_{run_name}".replace(" ", "").replace(".", "").replace("(", "").replace(")", "").lower() + "dd_" + str(des_dir_coef) + "_bb_" + str(search_box_len).replace(".", "_") + "_dist_" + str(nn_ft_dist).replace(".", "_") + "_width_" + str(nn_ft_width).replace(".", "_")
+        # python_cmd = f"python evaluate_policy.py --run_name='{run_name}' --debug"
+        python_cmd = run_name
 
-                    slurm_options = [
-                        "--cpus-per-gpu", "7",
-                        "-p", "overcap",
-                        "-A", "overcap",
-                        "--constraint", "rtx_6000|a40",
-                        "--gres", "gpu:8",
-                        "-x", get_blacklist() + ",cortana,fiona",
-                        #"-w", "zima, sophon, claptrap"
-                    ]
+        slurm_options = [
+            "--cpus-per-gpu", "7",
+            "-p", "overcap",
+            "-A", "overcap",
+            "--constraint", "rtx_6000|a40",
+            "--gres", "gpu:8",
+            "-x", get_blacklist() + ",cortana,fiona",
+            #"-w", "zima, sophon, claptrap"
+        ]
 
-                    # loop through random seeds
-                    # for i in range(num_runs):
-                    unique_name = job_nickname
-                    dep_slurm_args = [
-                        "-o",  unique_name + ".log",
-                        "-J", unique_name
-                    ]
-                    cmd = ["sbatch"] + slurm_options + dep_slurm_args + ["eval_submit.sh"] \
-                        + [python_cmd] + [unique_name] + [str(des_dir_coef)] + [str(search_box_len)] + [str(nn_ft_dist)] + [str(nn_ft_width)]
-                    print(" ".join(cmd))
-                    print()
-                    subprocess.run(cmd)
+        # loop through random seeds
+        # for i in range(num_runs):
+        unique_name = job_nickname
+        dep_slurm_args = [
+            "-o",  unique_name + ".log",
+            "-J", unique_name
+        ]
+        cmd = ["sbatch"] + slurm_options + dep_slurm_args + ["eval_submit.sh"] \
+            + [python_cmd] + [unique_name] + [str(des_dir_coef)] + [str(search_box_len)] + [str(nn_ft_dist)] + [str(nn_ft_width)]
+        print(" ".join(cmd))
+        print()
+        subprocess.run(cmd)
 
 
 if __name__ == "__main__":
