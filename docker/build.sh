b1fb234cbed15222a3767e9886c43553b351e5f9
Jeremiah Coholich <jcoholich@gatech.edu>
2022-07-09 10:29:59 -0400

    add automatic pruning whenever docker build fails, because its usually due to space

--- docker/build.sh
+++ docker/build.sh
@@ -4,3 +4,17 @@ set -u
 SCRIPTROOT="$( cd "$(dirname "$0")" ; pwd -P )"
 cd "${SCRIPTROOT}/../.."
 docker build --network host -t isaacgym_$1 -f isaacgym/docker/Dockerfile .
+if [[ $? !=  0 ]]
+then
+    echo Docker build failed, try pruning because space is typically an issue
+    yes | docker system prune
+    docker build --network host -t isaacgym_$1 -f isaacgym/docker/Dockerfile .
+fi
+
+if [[ $? !=  0 ]]
+then
+    echo Docker build failed, try pruning ALL because space is typically an issue
+    yes | docker system prune -a
+    docker build --network host -t isaacgym_$1 -f isaacgym/docker/Dockerfile .
+fi
+
