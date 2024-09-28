c029668e7b1647a3cd9a6c62f1b17b3eaa3bb837
Jeremiah Coholich <jcoholich@gatech.edu>
2024-09-28 16:14:48 -0400

    update script for running on skynet

--- submit.sh
+++ submit.sh
@@ -1,23 +1,9 @@
 #!/bin/bash -l
+#SBATCH --qos short
+#SBATCH -p kira-lab
+#SBATCH -c 8
+#SBATCH -G a40:1
 
-# if [[ $(docker info | sed -n '15 p') != " Server Version: 20.10.7" ]]; then
-#     echo On node $(hostname)
-#     echo docker version is: $(docker info | sed -n '15 p')
-#     echo exiting ...
-#     exit
-# fi
-hostname
-NUM_GPUS=$(nvidia-smi --query-gpu=name --format=csv,noheader | wc -l)
+conda activate rlgpu
 
-# cd ~/rl_games
-# git checkout master
-# git pull origin master
-
-# cd ~/isaacgym
-# git checkout master
-# git pull origin master
-
-# yes | docker system prune -a
-bash docker/build.sh $2
-docker rm isaacgym_container_$2
-bash docker/sbatch_run.sh "$1" $2 # this script needs to do all the work
+$1
